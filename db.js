6be1b91b969d2629a9c05d3ae1a3ce860d944758
Ayesha Liaquat Ali <agul6411@gmail.com>
2026-08-27 14:30:08 +0500

    fix the error

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const poolConfig = {
   host: process.env.DB_HOST || '127.0.0.1',
   port: parseInt(process.env.DB_PORT || '3306', 10),
   user: process.env.DB_USER || 'admin',
-  password: process.env.DB_PASSWORD || 'yourstrongpassword',
+  password: process.env.DB_PASSWORD || 'YourStrongPassword',
   database: process.env.DB_NAME || 'phone_parts_finder',
   waitForConnections: true,
   connectionLimit: 10,
